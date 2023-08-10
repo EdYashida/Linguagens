0a1fc999848e6ff94fbbe7e913954121b2413253
EdYashida <107697323+EdYashida@users.noreply.github.com>
2023-08-10 11:48:36 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -10,14 +10,19 @@ public class Carro extends Thread {
         this.categoria=categoria;
         this.ponte=ponte;
         
-        if(categoria=="carro"){  //aplica prioridade com base na categoria do carro
+        if(categoria=="carro"){//aplica prioridade com base na categoria do carro
             this.prioridade=5;
+            setPriority(prioridade);
         }
         else if(categoria=="viatura"){
             this.prioridade=7;
+            setPriority(prioridade);
+
         }
         else if(categoria=="ambulancia"){
             this.prioridade=10;
+            setPriority(prioridade);
+
         }     
     }
     
