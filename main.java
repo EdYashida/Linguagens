b035c5a516860c2c62bd80c71e2bd5536c591e39
EdYashida <107697323+EdYashida@users.noreply.github.com>
2023-08-10 11:49:05 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,33 +1,29 @@
+
 package produtor_consumidor;
 
-public class Ponte {
-    boolean disponivel; 
-    
-    public Ponte(){
-        this.disponivel=true;
-    }
-    
-    public synchronized void atravessa(String categoria){
-        try{
-           if(disponivel==false){
-               System.out.println(categoria+"ocupado");
-               wait();
-           }
-           disponivel=false;
-           System.out.println(categoria+" atravessando"+Thread.currentThread().getPriority());
-           for(int i=1;i<=10;i++){
-               System.out.print(i+" ");
-               Thread.sleep(10);
-           }
-           System.out.println("");
-           System.out.println("-------------------------------------------------------------------");
-           notifyAll();        
-        }
-        catch(InterruptedException e){}
+import java.lang.*;
+
+public class ProduzConsome {
+    public static void main(String[] args) throws InterruptedException {
+        Ponte ponte = new Ponte();
+        Carro ambulancia = new Carro("ambulancia",ponte);
+        Carro viatura1 = new Carro("viatura",ponte);
+        Carro viatura2 = new Carro("viatura",ponte);
+        Carro carro1 = new Carro("carro",ponte);
+        Carro carro2 = new Carro("carro",ponte);
+
+        ambulancia.start();
+        viatura1.start();
+        viatura2.start();
+        carro1.start();
+        carro2.start();
         
-        finally{
-                System.out.println(categoria+" a seguir");
-                disponivel=true;
-                }
-    }  
+        for(int j=1;j<=10;j++){
+        ambulancia.join();
+        viatura1.join();
+        viatura2.join();
+        carro1.join();
+        carro2.join();  
+        }
+    }
 }
